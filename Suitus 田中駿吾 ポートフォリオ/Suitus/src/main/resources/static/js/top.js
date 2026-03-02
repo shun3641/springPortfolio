3c45649b0f26a6596523f4c46f2aa20a9affb8d8
shun3641 <tanakaLearning15q@gmail.com>
2026-03-02 11:22:26 +0900

    Add files via upload

--- Suitus 田中駿吾 ポートフォリオ/Suitus/src/main/resources/static/js/top.js
+++ Suitus 田中駿吾 ポートフォリオ/Suitus/src/main/resources/static/js/top.js
@@ -18,20 +18,5 @@ $(function(){
 					
 				  };
 	      })
-		  let path = $(location).attr('pathname');
-		  let lastSlashIndex = path.lastIndexOf('/');
 
-		  // 最後の'/'以降の文字列を切り出す
-		  // lastIndexOf('/') + 1 とすることで、'/'自体を含めずに次の文字から取得できる
-		  let lastSegment = path.substring(lastSlashIndex + 1);
-		  console.log(lastSegment);
-		  
-		  
-		  //背景画像にしたい画像パスを列挙
-		  const images = {ball: 'useball.jpg',
-			shoes: 'useshoes.jpg',
-			training: 'useTrainingGoods.jpg',
-			cap: 'cap.jpg'
-		  };
-		  $("#useImage").attr('src','/image/' + images[lastSegment]);
 	  })
